85959edae13185088dfbac0d22ce8b8994885f39
BenjaminPrehn <57618388+BenjaminPrehn@users.noreply.github.com>
2021-05-20 13:08:22 +0200

    Setup of scripts and express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express(); 
+
+const port = process.env.PORT || 8080;
+
+
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log('There was an issue starting your application');
+    }
+    console.log('The app is running on:', port);
+})
