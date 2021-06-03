6a479200c8098b4ba39f5406c2df2fcedf744ac6
BenjaminPrehn <57618388+BenjaminPrehn@users.noreply.github.com>
2021-06-03 17:54:22 +0200

    Added a view modal for projects and updated the table with date values

--- models/project.js
+++ models/project.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const projectsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
     description: {
         type: String,
         required: true,
--- public/projects/projects.js
+++ public/projects/projects.js
@@ -11,10 +11,15 @@
                 $("#addData")
                     .append(
                         $("<tr> <td>" +
-                                projects.description 
+                                projects.title
                             + "</td> <td>" + 
                                 projects.status
                             + "</td> <td>" + 
+                                moment(projects.createdAt).format("YYYY-MM-DD")
+                            + "</td> <td>" + 
+                                moment(projects.updatedAt).format("YYYY-MM-DD")
+                            + "</td> <td>" + 
+                            "<a href='' onclick=\"viewProjectById('"+ projects._id +"')\" data-bs-toggle='modal' data-bs-target='#viewProjectModal'><span class='fas fa-eye'></span></a>" + 
                                 "<a href='' onclick=\"getProjectById('"+ projects._id +"')\" data-bs-toggle='modal' data-bs-target='#updateProjectModal'><span class='fas fa-edit'></span></a>" + 
                                 "<a href='' onclick=\"deleteProjectById('"+ projects._id +"')\"><span class='fas fa-trash-alt'></span></a>"
                         + "</td> </tr>"))
@@ -36,11 +41,37 @@ function getProjectById(id) {
                 dataType: "json"
             }).done(
                 function(project) {
-                console.log(project.description);
+
+                $("#title").text(project.title);
+
+                $("#titleUpdate").val(project.title);
                 $("#descriptionUpdate").val(project.description);
                 $("#statusUpdate").val(project.status);
 
                 $("#updateForm").attr("action", "/projects/" + project._id);
+            }
+            
+        );
+    } catch (error) {
+        alert("Error")
+        console.log(error);
+    }
+}
+
+// View a project by it's id
+function viewProjectById(id) {
+    try {
+           $.ajax({
+                method: "GET",
+                url: "/projects/" + id,
+                dataType: "json"
+            }).done(
+                function(project) {
+
+                $("#title").text(project.title);
+                $("#description").text(project.description);
+                $("#status").text(project.status);
+
             }
             
         );
